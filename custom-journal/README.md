fdc57c3b2a71e428b94038c3ba4055701e0aa23c
Freewolf2000 <phillip@everafter.games>
2020-12-31 14:37:15 -0700

    First Upload
    Original Module
    Added swade-core div

---
+++ custom-journal/README.md
@@ -0,0 +1,83 @@
+# FVTT Custom Journal Theming - The Journals of your Dreams!
+
+> **IF YOU ARE STILL ON 1.0.11 OR LOWER, YOU WILL MOST LIKELY HAVE TO REINSTALL MANUALLY, SINCE I MADE A MISTAKE IN THE MANIFEST LINK, BREAKING THE USUAL UPDATE PROCESS. SORRY ABOUT THE INCONVENIENCE!**
+
+Welcome to your own personal playground to design your own wonderful-looking journals! 
+Not only did I want to make journals more interesting, I also wanted to to allow them
+to be selectively themed, much like actors can. This is now possible, with a current 
+library of three themes in addition to the original Foundry look.
+
+![Preview](https://github.com/Sanyella/FVTT-Custom-Journal-Theming/raw/master/custom-journal.png)
+
+In addition to the regular reskin provided by the different themes, you can now also benefit from some other style options in the editor dropdown, such as drop caps (moved from defaulting to *every* paragraphs' first letter) and various text "boxes"! (*not supported by all themes)
+
+![Additional Options](https://github.com/Sanyella/FVTT-Custom-Journal-Theming/raw/master/custom-journal_more-options.png)
+![Block Demo](https://github.com/Sanyella/FVTT-Custom-Journal-Theming/raw/master/custom-journal_block-demo.png)
+
+## Known Issues
+
+If you encounter any bug or want to suggest improvements, don't hesitate to tell me either 
+here through the issues or on Discord (do tag me if reaching out through the Foundry Discord
+if you want me to see your message @Sanyella#3760). Do take into account though that this is
+still an early version which needs a lot more work. I'll do my best to fix these issues as 
+fast as possible, but take note that it may still take a certain time before I have the time 
+to fix something.
+
+- **Bug**: The drop down for the "default" journal sheet doesn't currently work. I have yet to 
+find out why, so please be patient. Rest assured, individual sheet themes *are* working.
+
+- **Compatibility issue**: The button for MINDMAP doesn't show when using Custom Journal. There
+is also a know issue with Data Toolbox.
+
+*Theme-specific issues aren't going to be listed here, but will be available and appropriately 
+tagged in the GitHub issues.*
+
+## Personalizing your journals
+
+If you want to customize the look of your journals, you can edit an existing theme,
+you can simply tweak values, or you can create your own theme, following the instructions
+in the styles/templates.css and the scripts/custom-journal.js files. Don't forget to add
+any extra file you create to the module.json as well so it loads.
+
+A final word of warning:
+BE WARNED: Since there isn't any way to save changes made to a module from updates
+(such as perhaps to introduce more themes), updating may mean ANY CHANGES YOU WOULD HAVE MADE WOULD BE LOST.
+
+> **PLEASE BACKUP ANY CHANGES YOU WANT TO KEEP OUTSIDE OF THE MODULE TO SET THOSE BACK IN CASE OF A MODULE UPDATE.**
+
+This is of course by no mean ideal, and I'll try to bring more themes natively to limit the 
+need for individual creation of such themes, but I do understand many will probably still 
+want to make their own, and as such I do try to guide you though it so my work can help you.
+Provided they do not introduce fonts or textures I cannot distribute and they are of good quality,
+I might also be open to adding your styles, if you so wish. Just reach out to me in Discord 
+(Sanyella#3760) or right here if you're confident to submit a pull request.
+
+## Thanks, attributions & links
+
+### Inspiration & Contributors
+
+People who have helped me in fixing various things within the module: Magnus/elizeuangelo, jakvike.
+
+Also shoutout to all those who have and still help me brainstorm and understand things in the Discord.
+
+**Big thanks to all of you! You rock!**
+
+Thanks to both Stryxin's dark mode module (https://github.com/Stryxin/dnd5edark-foundryvtt)
+and Felix's BetterNPCSheet module (https://github.com/syl3r86/BetterNPCSheet5e)
+which have both helped me tremendously in understanding how to structure my own
+module. Of course, also a huge thanks to Atropos for bringing Foundry VTT to life, in all its
+awesomness and in such an open way, with code easy to navigate and understand.
+
+### Fonts
+
+Lovers Quarrel font: https://fonts.google.com/specimen/Lovers+Quarrel
+
+Anglo-text font: https://www.1001fonts.com/anglo-text-font.html
+  
+### Textures
+
+Dark background 1: https://www.publicdomainpictures.net/en/view-image.php?image=169017&picture=grunge-background
+    
+Parchment 1, by FilterForge : https://www.flickr.com/photos/filterforge/9340122531
+
+Of course, wherever it may apply, all rights to other ressources used in this module are still their original owners', and I lay no claim to them whatsoever. 
---
+++ custom-journal/scripts/custom-journal.js
@@ -0,0 +1,288 @@
+// The custom parent class that allows the various setting needed to have
+// a choice of journals similar to what actor sheets get
+class CustomJournalSheet extends JournalSheet {
+
+
+	get journal(){
+		return this.object;
+	}
+
+	static get defaultOptions() {
+		const options = super.defaultOptions;
+		options.baseApplication = "JournalSheet";
+		options.classes.push('custom-journal');
+		return options;
+	}
+
+	//Include the option for the Drop Cap style in the editor styles' menu
+	//For compatibility with 6.6, to be removed when 7.x hits stable,
+	//replaced with activateEditor
+	_createEditor(target, editorOptions, initialContent) {
+		if (!editorOptions.style_formats) {
+			editorOptions.style_formats = [
+				{
+					title: "Custom",
+					items: [
+						{
+							title: "Secret",
+							block: 'section',
+							classes: 'secret',
+							wrapper: true
+						}
+					]
+				}
+			];
+		}
+		editorOptions.style_formats.push(
+			{
+				title: game.i18n.localize("custom-journal.StyleSection"),
+				items: [
+					{
+						title: game.i18n.localize("custom-journal.DropCap"),
+						inline: 'span',
+						classes: 'drop-cap'
+					},
+					{
+						title: game.i18n.localize("custom-journal.SWADECore"),
+						block: 'div',
+						classes: 'swade-core',
+						wrapper: true
+					},
+					{
+						title: game.i18n.localize("custom-journal.SimpleBlock"),
+						block: 'section',
+						classes: 'simple-block',
+						wrapper: true
+					},
+					{
+						title: game.i18n.localize("custom-journal.SimpleBlockFloat"),
+						block: 'section',
+						classes: 'simple-block-float',
+						wrapper: true
+					},
+					{
+						title: game.i18n.localize("custom-journal.RidgedBlock"),
+						block: 'section',
+						classes: 'ridged-block',
+						wrapper: true
+					},
+					{
+						title: game.i18n.localize("custom-journal.RidgedBlockFloat"),
+						block: 'section',
+						classes: 'ridged-block-float',
+						wrapper: true
+					}
+				]
+			}
+		);
+		super._createEditor(target, editorOptions, initialContent);
+	}
+
+	//Include the option for the Drop Cap style in the editor styles' menu
+	activateEditor(name, options={}, ...args) {
+		if (!options.style_formats)
+		{
+			options.style_formats = [
+				{
+					title: "Custom",
+					items: [
+						{
+							title: "Secret",
+							block: 'section',
+							classes: 'secret',
+							wrapper: true
+						}
+					]
+				}
+			];
+		}
+		options.style_formats.push(
+			{
+				title: game.i18n.localize("custom-journal.StyleSection"),
+				items: [
+					{
+						title: game.i18n.localize("custom-journal.DropCap"),
+						inline: 'span',
+						classes: 'drop-cap'
+					},
+					{
+						title: game.i18n.localize("custom-journal.SWADECore"),
+						block: 'div',
+						classes: 'swade-core',
+						wrapper: true
+					},
+					{
+						title: game.i18n.localize("custom-journal.SimpleBlock"),
+						block: 'section',
+						classes: 'simple-block',
+						wrapper: true
+					},
+					{
+						title: game.i18n.localize("custom-journal.SimpleBlockFloat"),
+						block: 'section',
+						classes: 'simple-block-float',
+						wrapper: true
+					},
+					{
+						title: game.i18n.localize("custom-journal.RidgedBlock"),
+						block: 'section',
+						classes: 'ridged-block',
+						wrapper: true
+					},
+					{
+						title: game.i18n.localize("custom-journal.RidgedBlockFloat"),
+						block: 'section',
+						classes: 'ridged-block-float',
+						wrapper: true
+					}
+				]
+			}
+		);
+		super.activateEditor(name, options, ...args);
+	}
+
+	/*
+	 *	Useful in creating a custom TinyMCE editor, to be looked into for further
+	 *	tinkering in that direction.
+	 */
+	// _createEditor(target, editorOptions, initialContent) {
+	// 	editorOptions.content_css = "./dark-slate-journal.css";
+	// 	return super._createEditor(target, editorOptions, initialContent);
+	// };
+
+	// Add the sheet configuration button to the journal header
+	_getHeaderButtons() {
+		let buttons = super._getHeaderButtons();
+
+		// Journal Configuration
+		const canConfigure = game.user.isGM || this.entity.owner;
+		if (this.options.editable && canConfigure) {
+			buttons = [
+				{
+					label: "Sheet",
+					class: "configure-sheet",
+					icon: "fas fa-cog",
+					onclick: ev => this._onConfigureSheet(ev)
+				}
+			].concat(buttons);
+		}
+		return buttons
+	}
+
+	// Allow the sheet configuration button to actually *do* something
+	_onConfigureSheet(event) {
+		event.preventDefault();
+		new EntitySheetConfig(this.journal, {
+			top: this.position.top + 40,
+			left: this.position.left + ((this.position.width - 400) / 2)
+		}).render(true);
+	}
+}
+
+/* CUSTOMIZE
+ * Add any extra themes here: just copy-paste the whole block, changing only the class
+ * name for the theme's name that will appear in the drop-down, and the name in single
+ * quotes (here, dark-slate-journal) with whatever name you gave your theme in the .css
+ * file
+ */
+class DarkModeJournal extends CustomJournalSheet {
+	static get defaultOptions() {
+		const options = super.defaultOptions;
+		options.classes.push('dark-mode-journal');
+		return options;
+	}
+}
+
+class DarkSlateJournal extends CustomJournalSheet {
+	static get defaultOptions() {
+		const options = super.defaultOptions;
+		options.classes.push('dark-slate-journal');
+		return options;
+	}
+}
+
+class HandwrittenLetter extends CustomJournalSheet {
+	static get defaultOptions() {
+		const options = super.defaultOptions;
+		options.classes.push('handwritten-letter');
+		return options;
+	}
+}
+
+class RoyalJournal extends CustomJournalSheet {
+	static get defaultOptions() {
+		const options = super.defaultOptions;
+		options.classes.push('royal-journal');
+		return options;
+	}
+}
+
+class SciFiOneJournal extends CustomJournalSheet {
+	static get defaultOptions() {
+		const options = super.defaultOptions;
+		options.classes.push('sci-fi-one-journal');
+		return options;
+	}
+}
+
+class SciFiTwoJournal extends CustomJournalSheet {
+	static get defaultOptions() {
+		const options = super.defaultOptions;
+		options.classes.push('sci-fi-two-journal');
+		return options;
+	}
+}
+
+// Creating the structure in CONFIG for Journals to have different sheets
+console.log("CustomJournals | Creating the structure to allow multiple Journal Sheets.")
+CONFIG["JournalEntry"]["sheetClasses"] = {};
+CONFIG["JournalEntry"]["sheetClasses"][CONST.BASE_ENTITY_TYPE] = {};
+
+console.log("CustomJournals | Registering the module's sheets.")
+
+/*CUSTOMIZE
+ * Here, register your sheet so it shows up properly in the dropdown, just change
+ * for your sheet name and you're good to go
+ */
+// The default Foundry journal
+EntitySheetConfig.registerSheet(JournalEntry, "journals", CustomJournalSheet, {
+	// label: game.i18n.localize("custom-Journal.CustomJournalSheet"),
+	types: [CONST.BASE_ENTITY_TYPE],
+	makeDefault: true
+});
+
+EntitySheetConfig.registerSheet(JournalEntry, "journals", DarkModeJournal, {
+	// label: game.i18n.localize("custom-Journal.DarkMode"),
+	types: [CONST.BASE_ENTITY_TYPE],
+	makeDefault: false
+});
+
+EntitySheetConfig.registerSheet(JournalEntry, "journals", DarkSlateJournal, {
+	// label: game.i18n.localize("custom-Journal.DarkSlate"),
+	types: [CONST.BASE_ENTITY_TYPE],
+	makeDefault: false
+});
+
+EntitySheetConfig.registerSheet(JournalEntry, "journals", HandwrittenLetter, {
+	// label: game.i18n.localize("custom-Journal.HandwrittenLetter"),
+	types: [CONST.BASE_ENTITY_TYPE],
+	makeDefault: false
+});
+
+EntitySheetConfig.registerSheet(JournalEntry, "journals", RoyalJournal, {
+	// label: game.i18n.localize("custom-Journal.Royal"),
+	types: [CONST.BASE_ENTITY_TYPE],
+	makeDefault: false
+});
+
+EntitySheetConfig.registerSheet(JournalEntry, "journals", SciFiOneJournal, {
+	// label: game.i18n.localize("custom-Journal.SciFiOne"),
+	types: [CONST.BASE_ENTITY_TYPE],
+	makeDefault: false
+});
+
+EntitySheetConfig.registerSheet(JournalEntry, "journals", SciFiTwoJournal, {
+	// label: game.i18n.localize("custom-Journal.SciFiTwo"),
+	types: [CONST.BASE_ENTITY_TYPE],
+	makeDefault: false
+});
